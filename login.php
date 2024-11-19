0a9179cc01470cefdc39cf396e7c801e807df4db
Patryk333 <kowalikpatryk06@gmail.com>
2024-11-19 09:24:58 +0100

    Update login.php

--- login.php
+++ login.php
@@ -2,4 +2,4 @@
 $user = $_POST["user"];
 $pwd = $_POST["pwd"];
 
-echo"El teu usuari es : " .$user. " i la teua contraseña es : " .$pwd;
+echo "El teu usuari es : " .$user. " i la teua contraseña es : " .$pwd;
